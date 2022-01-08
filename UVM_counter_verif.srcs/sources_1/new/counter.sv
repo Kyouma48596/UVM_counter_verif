3619554e2a7b8c9ff878943967e14f7c3c2a73af
Kyouma48596 <56397478+Kyouma48596@users.noreply.github.com>
2022-01-08 14:59:49 +0530

    Designed counter

---
+++ UVM_counter_verif.srcs/sources_1/new/counter.sv
@@ -0,0 +1,9 @@
+module counter(counter_if.cntr_mp cif);
+
+    always @(posedge cif.clk) begin
+        if(cif.rst) cif.q<=0;
+        else if(cif.ld) cif.q<=cif.data_in;
+        else if(cif.inc) cif.q<=cif.q+1;
+    end
+
+endmodule
