efde08d8077b2d4daebae2dbc6fd0d45fc3a69c8
Kyouma48596 <56397478+Kyouma48596@users.noreply.github.com>
2022-01-08 15:07:08 +0530

    Made counter_pkg

---
+++ UVM_counter_verif.srcs/sources_1/new/counter_pkg.sv
@@ -0,0 +1,9 @@
+package counter_pkg;
+
+    import uvm_pkg::*;
+    `include "uvm_macros.svh";
+    `include "counter_test.svh";
+    
+    virtual interface counter_if global_cif;
+    
+endpackage
